e9183986aa3c99a499d26bc71f84def45dada579
Rithin Skaria <rithin@rithin.net>
2019-07-15 08:41:14 +0530

    Update All-groups-and-users-csv.ps1

--- All-groups-and-users-csv.ps1
+++ All-groups-and-users-csv.ps1
@@ -11,3 +11,6 @@ $count = Get-AzureADGroupMember -ObjectId $id[$i] | Select-Object DisplayName
 $c=$count.Length
 Add-Content C:\Test\Report.txt "$($name[$i]) $($c)"
 }
+
+#To differentiate On-prem and Cloud users, Get-AzureADGroupMember -ObjectId $id[0] | Select-Object DisplayName,Mail,OnPremisesSecurityIdentifier 
+# Add OnPremisesSecurityIdentifier to the objects, only Directory Synced users will have a value, others will be null
