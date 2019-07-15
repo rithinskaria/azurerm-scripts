6abfaa670df9f251a9452686bc57da8ad746346e
rithinskaria <38252976+rithinskaria@users.noreply.github.com>
2019-07-15 08:25:48 +0530

    Create ignore-empty-groups-and-getusers.ps1

---
+++ ignore-empty-groups-and-getusers.ps1
@@ -0,0 +1,14 @@
+# Groups only with members to CSV with report
+Connect-AzureAD
+$group= Get-AzureADGroup
+$id= @($group.ObjectID)
+$name= @($group.DisplayName)
+for ($i=0; $i -le $id.Length; $i++){
+$path = $name[$i]
+$count = Get-AzureADGroupMember -ObjectId $id[$i] | Select-Object DisplayName
+$c=$count.Length
+if ($c -gt 0){
+Get-AzureADGroupMember -ObjectId $id[$i] | Select-Object DisplayName,Mail |Export-Csv -Path C:\Test\$path.csv -Append
+Add-Content C:\Test\Report.txt "$($name[$i]) $($c)"
+}
+}
