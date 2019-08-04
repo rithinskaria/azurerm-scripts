2509f18836f7dfc7551f6592aa534bfa8442029f
Rithin Skaria <rithin@rithin.net>
2019-08-04 08:56:38 +0530

    Update Get AAD roles and users.ps1

--- Get AAD roles and users.ps1
+++ Get AAD roles and users.ps1
@@ -7,7 +7,7 @@ Connect-AzureAD
 $group= Get-AzureADDirectoryRole # This will select only the roles which has assignments
 $id= @($group.ObjectID)
 $name= @($group.DisplayName)
-for ($i=0; $i -le $id.Length; $i++){
+for ($i=0; $i -lt $id.Length; $i++){
 $path = $name[$i]
 Get-AzureADDirectoryRoleMember -ObjectId $id[$i] | Export-Csv -Path C:\Test\$path.csv # First run and select the fields from raw CSV and then use Select-Object
 } 
