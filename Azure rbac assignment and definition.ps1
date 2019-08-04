d1192c498ee255ad5a985e57e3523f09ba47680d
Rithin Skaria <rithin@rithin.net>
2019-08-04 08:55:35 +0530

    Update Azure rbac assignment and definition.ps1

--- Azure rbac assignment and definition.ps1
+++ Azure rbac assignment and definition.ps1
@@ -4,3 +4,14 @@ Get-AzureRmRoleAssignment | Select-Object Scope,DisplayName,SignInName,RoleDefin
 
 #Get RBAC description
 Get-AzureRmRoleDefinition | Select-Object Name,Description #export-CSV if needed
+
+#If you have multiple subscriptions and would need report from each sub, use code below. Need at least Reader role in each sub or at management group level to get proper output
+
+Login-AzAccount
+$sub_list=Get-AzureRmSubscription
+$sub_id=$sub_list.Id
+foreach ($id in $sub_id){ 
+Set-AzureRmContext -Subscription $id
+Get-AzureRmRoleAssignment | select Scope,DisplayName,SignInName,ObjectType,RoleDefinitionName | Export-Csv -Path C:\rbac-reader.csv -Append 
+}
+
